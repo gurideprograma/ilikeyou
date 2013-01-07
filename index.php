5074e7039fecdbe212c958c889479e08b7020c44
Tiago <c4ffe1n3@poweredbylinux.(none)>
2013-01-07 19:21:35 -0200

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?
+include("inc/core.php");
+include("inc/def.php");
+?><html>
+	<head>
+		<title>i like you!</title>
+		
+		<script type="text/javascript" src="<?= DIR ?>/inc/jquery-1.5.1.js"></script>
+		<script type="text/javascript" src="<?= DIR ?>/inc/jqueryui/js/jquery-ui-1.8.10.custom.min.js"></script>
+		<script type="text/javascript" src="<?= DIR ?>/inc/jqueryui.js"></script>
+		
+		<link type="text/css" href="<?= DIR ?>/inc/jqueryui.css" rel="stylesheet" />
+		<link type="text/css" href="<?= DIR ?>/inc/css.css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
+		<link type="text/css" href="<?= DIR ?>/inc/jqueryui/css/redmond/jquery-ui-1.8.10.custom.css" rel="stylesheet" />
+		
+		<meta http-equiv="content-language" content="en">
+		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+		<meta name="description" content="" />
+		<meta name="keywords" content="">
+		<meta name="author" content="" />
+		<meta name="reply-to" content="">
+        	<meta name="google" content="notranslate" />
+
+        </head>
+        <body>
+        	<div id="title">i-<span>like</span>you<span id="ext">.net</div></div>
+        	
+        	<div id="cont">
+        		
+        	</div>
+        </body>
+</html>
